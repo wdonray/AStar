bbcb9849f08f1d8eac4f898c2f5af733c20abd2b
Donray Williams <w.donray@yahoo.com>
2017-03-31 16:37:23 -0500

    starting to move over to game loop

---
+++ constants.py
@@ -0,0 +1,13 @@
+"""All constants for game."""
+import pygame
+
+PINK = (255, 153, 255)
+MAROON = (128, 0, 0)
+YELLOW = (255, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+CLOCK = pygame.time.Clock()
+SCREEN = pygame.display.set_mode((800, 600))
